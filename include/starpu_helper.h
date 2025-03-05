78be5406d8c100f52eaa34d841b243acc2001526
OOI Brian Jian Wei <booi@3a401-13.int-evry.fr>
2025-03-05 18:26:09 +0100

    Fixed axpy for high N

--- include/starpu_helper.h
+++ include/starpu_helper.h
@@ -4,5 +4,7 @@
 #include "starpu.h"
 
 double starpu_timing_now(void);
+ssize_t read_exact(int fd, void *buf, size_t size);
+ssize_t write_exact(int fd, const void *buf, size_t size);
 
 #endif // __STARPU_HELPER_H__
